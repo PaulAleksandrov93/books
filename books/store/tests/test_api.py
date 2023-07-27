3ad685a587e1beb4db2759ce584cb7f808d0edc6
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-07-27 22:50:49 +0500

    Added new unit-tests for testing CRUD in app

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from store.models import Book
 from store.serializers import BooksSerializer
-
+import json
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='author 1')
         self.book_2 = Book.objects.create(name='Test book 2', price=55, author_name='author 5')
         self.book_3 = Book.objects.create(name='Test book author 1', price=55, author_name='author 2')
@@ -20,7 +22,7 @@ class BooksApiTestCase(APITestCase):
 
     def test_get_filter(self):
         url = reverse('book-list')
-        response = self.client.get(url, data={'search': 55})
+        response = self.client.get(url, data={'price': 55})
         serializer_data = BooksSerializer([self.book_2, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
@@ -31,4 +33,21 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BooksSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_update(self):
+
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name
+        }
+        self.client.force_login(self.user)
+        json_data = json.dumps(data)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        # self.book_1 = Book.objects.get(id=self.book_1.id)
+        self.book_1.refresh_from_db()
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(575, self.book_1.price)
+
 
