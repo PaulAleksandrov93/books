39a500dfa8c07cf6e63dd45dfb34e9656d0f9823
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-07-28 20:56:17 +0500

    Implemented permissions on objects in DRF. Added new tests to check the functionality

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 from rest_framework.test import APITestCase
 
 from store.models import Book
@@ -10,7 +11,7 @@ import json
 class BooksApiTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='test_username')
-        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='author 1')
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='author 1', owner=self.user)
         self.book_2 = Book.objects.create(name='Test book 2', price=55, author_name='author 5')
         self.book_3 = Book.objects.create(name='Test book author 1', price=55, author_name='author 2')
     def test_get(self):
@@ -34,8 +35,21 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
-    def test_update(self):
+    def test_create(self):
+        url = reverse('book-list')
+        data = {
+            "name": "Programming in Python 3",
+            "price": 150,
+            "author_name": "Mark Summerfield"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+        self.assertEqual(self.user, Book.objects.last().owner)
 
+    def test_update(self):
         url = reverse('book-detail', args=(self.book_1.id,))
         data = {
             "name": self.book_1.name,
@@ -48,6 +62,40 @@ class BooksApiTestCase(APITestCase):
         # self.book_1 = Book.objects.get(id=self.book_1.id)
         self.book_1.refresh_from_db()
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(575, self.book_1.price)
+
+    def test_update_not_owner(self):
+        self.user2 = User.objects.create(username='test_username2')
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name
+        }
+        self.client.force_login(self.user2)
+        json_data = json.dumps(data)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        # self.book_1 = Book.objects.get(id=self.book_1.id)
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual({'detail': ErrorDetail(string='You do not have permission to perform this action.',
+                                                code='permission_denied')}, response.data)
+        self.assertEqual(25, self.book_1.price)
+
+    def test_update_not_owner_but_staff(self):
+        self.user2 = User.objects.create(username='test_username2', is_staff=True)
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name
+        }
+        self.client.force_login(self.user2)
+        json_data = json.dumps(data)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        # self.book_1 = Book.objects.get(id=self.book_1.id)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1.refresh_from_db()
         self.assertEqual(575, self.book_1.price)
 
 
